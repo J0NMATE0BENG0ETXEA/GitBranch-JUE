e64c43b7e83b80f626604f292dfb049df3ffa58c
Jon <jon.mateobe@elorrieta-errekamari.com>
2019-03-12 13:05:48 +0100

    gg

--- Mathew/src/liburutegia/NAGUSIA.java
+++ Mathew/src/liburutegia/NAGUSIA.java
@@ -3,7 +3,9 @@ package liburutegia;
 public class NAGUSIA {
 
 	public static void main(String[] args) {
-
+		Liburutegia biltegi = new Liburutegia(null, null, 0, null);
+		Liburua laselva = new Liburua(null, null, 0);
+		biltegi.GehituLiburua(laselva, 0);
 
 	}
 
