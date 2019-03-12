9199102cd38fc77647b58ea86a6a43d3adec641d
ErLoriz <erlantz.lorizab@elorrieta-errekamari.com>
2019-03-12 12:54:54 +0100

    hola

--- Mathew/src/liburutegia/Liburua.java
+++ Mathew/src/liburutegia/Liburua.java
@@ -1,5 +1,40 @@
 package liburutegia;
 
+import java.util.ArrayList;
+
 public class Liburua {
-//sfjidfg
+
+	private String izena;
+	private String helbidea;
+	private int ikasle_kop;
+	private ArrayList<Liburua> Liburuak = new ArrayList<Liburua>();
+	
+	
+	public String getIzena() {
+		return izena;
+	}
+	public void setIzena(String izena) {
+		this.izena = izena;
+	}
+	public String getHelbidea() {
+		return helbidea;
+	}
+	public void setHelbidea(String helbidea) {
+		this.helbidea = helbidea;
+	}
+	public int getIkasle_kop() {
+		return ikasle_kop;
+	}
+	public void setIkasle_kop(int ikasle_kop) {
+		this.ikasle_kop = ikasle_kop;
+	}
+	public ArrayList<Liburua> getLiburuak() {
+		return Liburuak;
+	}
+	public void setLiburuak(ArrayList<Liburua> liburuak) {
+		Liburuak = liburuak;
+	}
+	
+	
+	
 }
--- Mathew/src/liburutegia/Liburutegia.java
+++ Mathew/src/liburutegia/Liburutegia.java
@@ -1,5 +1,31 @@
 package liburutegia;
 
 public class Liburutegia {
-//jkfgjkhfg
+
+	private String izenburua;
+	private String ISBN;
+	private int orri_kop;
+	
+	
+	public String getIzenburua() {
+		return izenburua;
+	}
+	public void setIzenburua(String izenburua) {
+		this.izenburua = izenburua;
+	}
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+	public int getOrri_kop() {
+		return orri_kop;
+	}
+	public void setOrri_kop(int orri_kop) {
+		this.orri_kop = orri_kop;
+	}
+	
+	
+	
 }
