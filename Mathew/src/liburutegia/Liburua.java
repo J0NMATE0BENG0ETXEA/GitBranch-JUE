eb6726d072cc241bd1cbba8c1cb9b2d65eb63d23
urtzi <urtzi.etxebarriar@elorrieta-errekamari.com>
2019-03-12 12:42:26 +0100

    kodea

--- Mathew/src/liburutegia/Liburua.java
+++ Mathew/src/liburutegia/Liburua.java
@@ -1,5 +1,27 @@
 package liburutegia;
 
 public class Liburua {
-//sfjidfg
+private String izenburua;
+private String ISBN;
+private int orrialdeKopurua;
+
+public String getIzenburua() {
+	return izenburua;
+}
+public void setIzenburua(String izenburua) {
+	this.izenburua = izenburua;
+}
+public String getISBN() {
+	return ISBN;
+}
+public void setISBN(String iSBN) {
+	ISBN = iSBN;
+}
+public int getOrrialdeKopurua() {
+	return orrialdeKopurua;
+}
+public void setOrrialdeKopurua(int orrialdeKopurua) {
+	this.orrialdeKopurua = orrialdeKopurua;
+} 
+
 }
