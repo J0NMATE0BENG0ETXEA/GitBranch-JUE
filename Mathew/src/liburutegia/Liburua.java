f4782accc34db51338060492080d9552d5257a45
Jon <jon.mateobe@elorrieta-errekamari.com>
2019-03-12 12:45:39 +0100

    aldaketa

--- Mathew/src/liburutegia/Liburua.java
+++ Mathew/src/liburutegia/Liburua.java
@@ -1,5 +1,34 @@
 package liburutegia;
 
 public class Liburua {
-//sfjidfg
+	//Atributuak
+	private String izenburua;
+	private String ISBN;
+	private int orrialdeKopurua;
+
+	public Liburua(String izenburua, String iSBN, int orrialdeKopurua) {
+		this.izenburua = izenburua;
+		ISBN = iSBN;
+		this.orrialdeKopurua = orrialdeKopurua;
+	} 
+	
+	//Getter eta Setter
+	public String getIzenburua() {
+		return izenburua;
+	}
+	public void setIzenburua(String izenburua) {
+		this.izenburua = izenburua;
+	}
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+	public int getOrrialdeKopurua() {
+		return orrialdeKopurua;
+	}
+	public void setOrrialdeKopurua(int orrialdeKopurua) {
+		this.orrialdeKopurua = orrialdeKopurua;
+	}
 }
--- Mathew/src/liburutegia/Liburutegia.java
+++ Mathew/src/liburutegia/Liburutegia.java
@@ -1,10 +1,52 @@
 package liburutegia;
 
 public class Liburutegia {
-// eta Liburuak:ArrayList&lt;Liburua&gt;
+	//Atributuak.
 	private String Izena;
 	private String helbidea;
 	private int IkasleKopurua;
 	private Liburua [] Liburuak;
+	
+	public Liburutegia(String izena, String helbidea, int ikasleKopurua, Liburua[] liburuak) {
+		Izena = izena;
+		this.helbidea = helbidea;
+		IkasleKopurua = ikasleKopurua;
+		Liburuak = liburuak;
+	}
+
+	//Getter eta Setter
+	public String getIzena() {
+		return Izena;
+	}
+
+	public String getHelbidea() {
+		return helbidea;
+	}
+
+	public int getIkasleKopurua() {
+		return IkasleKopurua;
+	}
+
+	public Liburua[] getLiburuak() {
+		return Liburuak;
+	}
+
+	public void setIzena(String izena) {
+		Izena = izena;
+	}
+
+	public void setHelbidea(String helbidea) {
+		this.helbidea = helbidea;
+	}
+
+	public void setIkasleKopurua(int ikasleKopurua) {
+		IkasleKopurua = ikasleKopurua;
+	}
+
+	public void setLiburuak(Liburua[] liburuak) {
+		Liburuak = liburuak;
+	}
+	
+	
 	
 }
